da0195da2adbda73b44064968413baa66141e9ef
Gvantsa Shakarashvili <gvantsa.shakarashvili@quantori.com>
2022-05-06 13:18:38 +0400

    added quantiti check & check for button text

--- Gvantsa/auth.spec.js
+++ Gvantsa/auth.spec.js
@@ -9,7 +9,6 @@ function login() {
     cy.get('#user-name').type('standard_user')
     cy.get('#password').type('secret_sauce')
     cy.get('#login-button').click()
-
 }
 
 describe('My first test', function() {
@@ -26,7 +25,11 @@ describe('My first test', function() {
     it('checks items in the cart', function() {
         login()
         cy.get('#add-to-cart-sauce-labs-bike-light').click()
-        cy.get('.shopping_cart_link').click()
+        cy.get('.shopping_cart_link').click().then((el) => {
+            const text = el.text();
+            expect(text).to.eq("1")
+
+        })
     })
 
     it('checks checkout function', function() {
@@ -46,6 +49,10 @@ describe('My first test', function() {
         cy.get('#postal-code').type('1400')
         cy.get('#continue').click()
         cy.get('#finish').click()
+    })
+
+    it('Checks text of the button', function() {
+        cy.get('#back-to-products').should('have.text', 'Back Home')
     })
 
 

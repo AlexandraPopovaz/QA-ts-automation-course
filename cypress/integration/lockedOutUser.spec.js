d5f1c1823ffe5875219f077a48ada1dbe6df097b
maxim.chetverikov <maxim.chetverikov@quantori.com>
2022-05-15 23:39:07 +0300

    added problemUser test

--- cypress/integration/lockedOutUser.spec.js
+++ cypress/integration/lockedOutUser.spec.js
@@ -11,6 +11,7 @@ describe('Locked Out User', function (){
 
     it('Should check cant login locked user', function () {
         cy.get('[data-test="error"]').should("be.visible")
+        // Clean form
         cy.get('.error-button').click();
         cy.get('[data-test="username"]').clear();
         cy.get('[data-test="password"]').clear();
--- cypress/integration/problemUser.spec.js
+++ cypress/integration/problemUser.spec.js
@@ -10,10 +10,23 @@ describe('Problem user', function (){
     });
 
     it('Should check correct tile view', function () {
+        const title = {
+            inventoryTitleName: "",
+            inventoryDetailsName : "Sauce Labs Fleece Jacket"
+        }
         cy.get('#item_4_img_link').should("to.be.visible");
-        cy.get('#item_4_title_link > .inventory_item_name').contains("Sauce Labs Backpack");
+        cy.get('#item_4_title_link > .inventory_item_name')
+            .should("be.visible")
+            .and("contain.text", title.inventoryTitleName)
         cy.get('#item_4_img_link').click();
-        cy.get('.inventory_details_img').should("to.be.visible");
-        cy.get('.inventory_details_name').contains("Sauce Labs Fleece Jacket");
-    });
+        cy.get('.inventory_details_img').should("to.be.visible")
+        cy.get('.inventory_details_name')
+            .should("be.visible")
+            .and("contain.text", title.inventoryDetailsName)
+
+        if (title.inventoryTitleName !== title.inventoryDetailsName) {
+            // throw onerror
+            return false
+        }
+    })
 })

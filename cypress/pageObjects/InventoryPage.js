969ef0ec47ef9fbcdc24a57fe48e6ccde102ec6d
wade smirnov <wade@email.com>
2022-06-01 19:54:13 +0400

    model + class with empty methods

---
+++ cypress/pageObjects/InventoryPage.js
@@ -0,0 +1,14 @@
+export class InventoryPage {
+    async getListOfItemsWithPrices() {
+        return;
+    }
+    async selectItem() {}
+    async addItemToCart() {}
+    async removeItemFromCart() {}
+    async getAmountOfItemsinCart() {}
+    async sortItemsByName() {}
+    async sortItemsByPrice() {}
+    async openSideMenu() {}
+    async selectOptionFromSideMenu() {}
+    async selectSocialMedia() {}
+}
